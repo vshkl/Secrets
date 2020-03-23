deb72d253bbc38d8ae7da72db8156058eb7bb3ee
Pavel Vashkel <p.vashkel@datarockets.com>
2020-03-23 11:36:58 +0300

    Extract MainNavigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,10 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { Close } from './resources/icons'
 
-import AddFileScreen from './screens/AddFileScreen'
-import AddNoteScreen from './screens/AddNoteScreen'
-import HomeScreen from './screens/HomeScreen'
-
-const Stack = createStackNavigator()
+import { MainNavigator } from './navigation'
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="AddFile"
-          component={AddFileScreen}
-          options={{
-            title: 'New file',
-            headerBackImage: () => (
-              <Close color="#000000" height={24} width={24}/>
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="AddNote"
-          component={AddNoteScreen}
-          options={{
-            title: 'New note',
-            headerBackImage: () => (
-              <Close color="#000000" height={24} width={24}/>
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Home' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MainNavigator/>
   )
 }
 
